01cd7dcac55e95a89468836e794814b5e05cf43b
Kapil Cadana <kb@cadana.dk>
2025-07-03 12:54:14 +0530

    student controller updated

--- backend/src/modules/students/students-controller.js
+++ backend/src/modules/students/students-controller.js
@@ -2,34 +2,43 @@ const asyncHandler = require("express-async-handler");
 const { getAllStudents, addNewStudent, getStudentDetail, setStudentStatus, updateStudent } = require("./students-service");
 
 const handleGetAllStudents = asyncHandler(async (req, res) => {
-    //write your code
-
+    const students = await getAllStudents(req.body);
+    res.json({students});
 });
 
 const handleAddStudent = asyncHandler(async (req, res) => {
-    //write your code
+    const message = await addNewStudent(req.body);
 
+    res.json(message);    
 });
 
 const handleUpdateStudent = asyncHandler(async (req, res) => {
-    //write your code
-
+    const message = await updateStudent(req.body);
+    res.json(message);
 });
 
 const handleGetStudentDetail = asyncHandler(async (req, res) => {
-    //write your code
+    const { id } = req.params;
+    const message = await getStudentDetail(id);
 
+    res.json(message); 
 });
 
 const handleStudentStatus = asyncHandler(async (req, res) => {
-    //write your code
+    const { status } = req.body;
+    const { id } = req.params;
 
+    const message = await setStudentStatus(
+        { userId: id, reviewerId: null, status }
+    );
+
+    res.json(message);
 });
 
 module.exports = {
-    handleGetAllStudents,
-    handleGetStudentDetail,
-    handleAddStudent,
-    handleStudentStatus,
-    handleUpdateStudent,
+    handleGetAllStudents, // done
+    handleGetStudentDetail, // done
+    handleAddStudent, // done
+    handleStudentStatus, // done
+    handleUpdateStudent, // done
 };
